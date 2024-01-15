de9534cee32fbf275dfb881b9e27ab05105d9497
Thusi101 <moraseroka10@gmail.com>
2024-01-15 15:52:44 -0800

    100

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 import sys
-
-message = "and that piece of art is useful - DORA Korpar, 2015-10-19\n"
-sys.stderr.write(message)
+sys.stderr.write("and that piece of art is useful - DORA Korpar, 2015-10-19\n")
 sys.exit(1)
