23006b18abe419e487a377b7911f2f6e47da5716
Thusi101 <moraseroka10@gmail.com>
2024-01-16 11:47:46 -0800

    11

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """
+    Computes a to the power of b and returns the value.
+
+    :param a: Base
+    :param b: Exponent
+    :return: Result of a raised to the power of b
+    """
+    return a ** b
