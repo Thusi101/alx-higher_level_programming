038723fcfd1ed5aa839df496b431f2df94820ee7
Thusi101 <moraseroka10@gmail.com>
2024-01-16 11:42:51 -0800

    10

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a, b):
+    """
+    Adds two integers and returns the result.
+
+    :param a: First integer
+    :param b: Second integer
+    :return: Sum of a and b
+    """
+    return a + b
