f7bae3bef47448388a7afdc504696978986ca521
Thusi101 <moraseroka10@gmail.com>
2024-01-18 13:57:44 -0800

    5

---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from variable_load_5 import a
+
+def print_variable():
+    # Print the value of the variable imported from variable_load_5.py
+    print("The value of the imported variable 'a' is:", a)
+
+if __name__ == "__main__":
+    print_variable()
