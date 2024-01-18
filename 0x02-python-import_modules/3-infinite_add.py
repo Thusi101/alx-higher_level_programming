500f8bf9ea78ca472684266f6e7837ed154d1e5f
Thusi101 <moraseroka10@gmail.com>
2024-01-18 05:35:14 -0800

    fixedsum

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-
-    def add_arg(argv):
-        add_arg(sys.argv)
-        n = len(argv) - 1
-        if n == 0:
-            print("{:d}".format(n))
-            return
-        else:
-            i = 1
-            add = 0
-            while i <= n:
-                add += int(argv[i])
-                i += 1
-                print("{:d}".format(add))
+    from sys import argv
+    sumint = 0
+    if len(argv) < 2:
+        print("0")
+    else:
+        equate = sum(int(arg) for arg in argv[1:])
+        print(equate)
