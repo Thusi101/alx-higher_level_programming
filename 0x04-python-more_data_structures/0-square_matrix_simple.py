d76d67547b635ca5a146eb670ce57725f62b7511
Thusi101 <moraseroka10@gmail.com>
2024-01-24 05:53:00 -0800

    0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,5 +5,7 @@
     :param matrix: 2-dimensional array
     :return: New matrix with squared values
     """
+
 def square_matrix_simple(matrix=[]):
+
     return [[y ** 2 for y in x] for x in matrix]
