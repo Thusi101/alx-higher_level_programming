d1a03f9ca91ef656c401934d3ba800972250c3a2
Thusi101 <moraseroka10@gmail.com>
2024-02-05 14:31:05 -0800

    fixed spacing and div error with append function

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,18 +2,18 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    try:
-        for itm in range(list_length):
-            try:
-                new.append(my_list_1[itm] / my_list_2[itm])
-            except ZeroDivisionError:
-                new.append(0)
-                print("division by 0")
-            except TypeError:
-                new.append(0)
-                print("wrong type")
-            except IndexError:
-                new.append(0)
-                print("out of range")
-    finally:
-        return new_list
+    for itm in range(list_length):
+        try:
+            div = my_list_1[itm] / my_list_2[itm]
+        except ZeroDivisionError:
+            div = 0
+            print("division by 0")
+        except TypeError:
+            div = 0
+            print("wrong type")
+        except IndexError:
+            div = 0
+            print("out of range")
+        finally:
+            new_list.append(div)
+    return new_list
