9b1de27924f425b8a9bf900e14ed7fb0dd91cd00
Thusi101 <moraseroka10@gmail.com>
2024-02-19 02:19:06 -0800

    fixed line too long

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,5 +4,6 @@ Defines an object attribute and returns a list of lookup function."""
 
 
 def lookup(obj):
-    """Return a list of an available object attributes and methods of the object."""
+    """Return a list of an available object attributes
+    and methods of the object."""
     return (dir(obj))
