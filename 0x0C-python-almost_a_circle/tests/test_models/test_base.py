c72f93e00bd1548c48082533f87e165ba73a98ba
Thusi101 <moraseroka10@gmail.com>
2024-03-01 13:43:28 -0800

    unttest

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 '''Module for Base unit tests.'''
 import unittest
+import os
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
 
 
 class TestBase(unittest.TestCase):
-    '''Tests the Base class.'''
+    '''Unittests Testing the Base class.'''
 
     def setUp(self):
         '''Imports module, instantiates class'''
