9efca9d3e0e291ab0c3c1515a5bb97b6f30a2415
Thusi101 <moraseroka10@gmail.com>
2024-06-04 20:59:44 -0700

    k

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 
-const args = process.argv.slice(2);
-
-fs.readFile(args[0], 'utf-8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) console.log(err);
+  else console.log(data);
 });
